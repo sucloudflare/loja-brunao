e8919bb840e20c5262a652de74aba7668542260c
Edson Bruno <cloudflare.ddos21@gmail.com>
2025-01-05 10:32:20 -0300

    paaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/data/athletes.ts
+++ src/data/athletes.ts
@@ -2,7 +2,7 @@ export const athletes = [
   {
     id: 1,
     name: 'Neymar Jr',
-    image: 'https://images.unsplash.com/photo-1577223625816-7546f13df25d',
+    image: 'https://img-prod.sportmediaset.mediaset.it/images/2023/03/09/101822041-b2bcf95f-a6c9-4fb9-9b2e-bae44de4eebf.jpg',
     description: 'Embaixador global da marca e ícone do futebol brasileiro.',
     instagram: 'https://instagram.com/neymarjr',
     twitter: 'https://twitter.com/neymarjr'
@@ -10,7 +10,7 @@ export const athletes = [
   {
     id: 2,
     name: 'Lionel Messi',
-    image: 'https://images.unsplash.com/photo-1630507144156-66f870d7c9e6',
+    image: 'https://www.hdwallpapers.in/download/lionel_messi_4k-1440x900.jpg',
     description: 'O maior jogador de todos os tempos, 8 vezes melhor do mundo.',
     instagram: 'https://instagram.com/leomessi',
     twitter: 'https://twitter.com/teammessi'
@@ -18,7 +18,7 @@ export const athletes = [
   {
     id: 3,
     name: 'Cristiano Ronaldo',
-    image: 'https://images.unsplash.com/photo-1630395822970-acd6a691d97e',
+    image: 'https://2.bp.blogspot.com/-B7Q6smFgNlE/V1bjnZCQFqI/AAAAAAAABCA/vB_sAlst6rUAbO71FY6HE0NqSzxeN28sQCLcB/s1600/cr7_47.jpg',
     description: 'Lenda do futebol e símbolo de dedicação e excelência.',
     instagram: 'https://instagram.com/cristiano',
     twitter: 'https://twitter.com/cristiano'
