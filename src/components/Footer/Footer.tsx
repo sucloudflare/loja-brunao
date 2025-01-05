47fc125eae272ee986dbbe2d6561a2233cdbc6f6
Edson Bruno <cloudflare.ddos21@gmail.com>
2025-01-05 11:08:34 -0300

    apppppdjjjjjjjjjjfbbbbbbbb

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,8 @@ import { NewsletterForm } from '../Newsletter/NewsletterForm';
 
 export function Footer() {
   return (
-    <footer className="bg-zinc/95 w-full z-50 h-screen flex text-white py-16">
+    <footer className="bg-zinc/95 backdrop-blur-sm h-screen flex fix
+ed w-full z-50 text-white py-16">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
           <div>
