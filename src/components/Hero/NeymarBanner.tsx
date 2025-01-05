1be648642450f9c0ca9fd0815352ebf90cdd9d4b
Edson Bruno <cloudflare.ddos21@gmail.com>
2025-01-05 13:38:01 -0300

    apapsidhdhfhffhfhfhfhfhfhhf

--- src/components/Hero/NeymarBanner.tsx
+++ src/components/Hero/NeymarBanner.tsx
@@ -25,6 +25,20 @@ export function NeymarBanner() {
           </div>
         </div>
       </div>
+      <div className="relative h-screen w-full">
+  {/* Vídeo oculto como fundo com áudio */}
+  <iframe
+    className="absolute inset-0 w-full h-full object-cover opacity-0"
+    src="https://www.youtube.com/embed/s4oA5CRC1us?autoplay=1&loop=1&playlist=s4oA5CRC1us&controls=0&showinfo=0&modestbranding=1&rel=0&mute=0"
+    title="Video Banner"
+    frameBorder="0"
+    allow="autoplay; fullscreen"
+  />
+
+  {/* Overlay com opacidade */}
+  <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/50 to-transparent" />
+</div>
+
     </div>
   );
 }
